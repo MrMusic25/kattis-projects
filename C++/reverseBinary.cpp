ec3f64051c3391e2f02ba34237bd389024c2fcb9
Kyle Krattiger <mr.music25@gmail.com>
2017-09-19 13:03:11 -0700

    Thought it was working, but only passed two test cases. Must be missing something. Wioll revisit at a later date.

--- C++/reverseBinary.cpp
+++ C++/reverseBinary.cpp
@@ -1,66 +1,60 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 // EVERYTHING IS FIFO
+int pow(int x, int y) {
+	if (y == 0) return 1;
+	return x * pow(x,y-1);
+}
 
-void i2b(int num, int rep/*resentation*/[]) {
-	int n = num, size = ((int)sizeof(rep)/sizeof(rep[0]));
-	for (int i = 0, j = size; i < size; i++, j--) {
-		if (n > pow(2,j) - 1) {
-			n -= pow(2,j) - 1;
+void i2b (int rep[], int num, int size) {
+	int /*size = (int)(sizeof(rep[])/sizeof(rep[0])),*/ pwr = 0, original = num;
+	//cout << "Size=" << size << endl;
+	for (int i = 0, j = size - 1; i < size; i++, j--) {
+		pwr = pow(2,j);
+		if (original >= pwr) {
+			original -= pwr;
 			rep[i] = 1;
 		}
 	}
 }
 
-void reverseArray(int rep[]) {
-	int temp = -1, size = ((int)sizeof(rep)/sizeof(rep[0]));
-	for (int i = 0, j = size - 1; i != ((int)(size/2) - 1); i++, j--) {
-		temp = rep[i];
-		rep[i] = rep[j];
-		rep[j] = temp;		
-	}
-}
-
-int b2i(int rep[]) {
-	int sum = 0, size = ((int)sizeof(rep)/sizeof(rep[0]));
+int b2i (int rep[], int size) {
+	int /*size = (int)(sizeof(rep)/sizeof(rep[0])),*/ sum = 0;
 	for (int i = 0, j = size - 1; i < size; i++, j--) {
 		if (rep[i] == 1) sum += pow(2,j);
 	}
 	return sum;
 }
 
-int nsum(int n) {
-	if (n == 0) return 1;
-	return pow(2,n) + nsum(n-1);
-}
-
-void outputArray(int rep[]) {
-	int size = ((int)sizeof(rep)/sizeof(rep[0]));
-	cout << "Binary=";
-	for (int i = 0; i < size; i++) cout << rep[i];
-	cout << endl;
+void revArray(int rep[], int size) {
+	int /*size = (int)(sizeof(rep)/sizeof(rep[0])),*/ i, temp;
+	if (size%2 == 0)
+		i = (int)(size/2);
+	else
+		i = (int)((size-1)/2);
+	
+	for (int j = 0, k = size - 1; j < i; j++, k--) {
+		temp = rep[j];
+		rep[j] = rep[k];
+		rep[k] = temp;
+	}
 }
 
 int main() {
-	int input, output;
+	int input;
 	cin >> input;
 	if (input < 1 || input > 1000000000) return 1;
-	//input--;
 	
-	int i = -1;
-	do {
-		i++;
-		cout << "nsum(" << i << ")=" << nsum(i) << endl;
-	} while(nsum(i) <= input);
-	int binary[i];
-	for (int j = 0; j < i; j++) binary[j] = 0;
+	int size = 0;
+	while (pow(2,size) < input) size++;
+	//size++;
+	int binary[size];
+	for (int i = 0; i <= size; i++) binary[i] = 0;
 	
-	i2b(input,binary);
-	outputArray(binary);
-	reverseArray(binary);
-	outputArray(binary);
-	cout << b2i(binary) << endl;
+	i2b(binary, input, size);
+	revArray(binary, size);
+	
+	cout << b2i(binary,size) << endl;
 	return 0;
 }
