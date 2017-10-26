4a129aad7565bce9da2af4c37f7f49f6acf67860
Kyle Krattiger <mr.music25@gmail.com>
2017-10-26 16:17:46 -0700

    Added another overloaded function to the template. Accepted grassseed.cpp

--- C++/grassseed.cpp
+++ C++/grassseed.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -51,7 +52,19 @@ bool withinBounds(string value, int max, char type) {
 }
 
 int main() {
+	double c;
+	int l;
+	cin >> c >> l;      // 8 precesion, according to website
+	if (!(withinBounds((double)0.00000001,(double)100.0,c,'e') && withinBounds(1,100,l,'e'))) return 1;
 	
+	double input[2], results;
+	for (int i = 0; i < l; i++) {
+		cin >> input[0] >> input[1];
+		//if (!(withinBounds((double)0.0,(double)100.0,input[0],'e') && withinBounds((double)0.0,(double)100.0,input[1],'e'))) return 1;
+		results += (input[0] * input[1]) * c;
+	}
+	
+	cout << fixed << setprecision(6) << results << endl;
 	
 	return 0;
 }
